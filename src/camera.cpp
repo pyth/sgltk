2c90191fb24398d1acfdcc31d847020c5797db0b
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2015-12-09 15:44:21 +0100

    orthographic projection

--- src/camera.cpp
+++ src/camera.cpp
@@ -2,15 +2,33 @@
 
 Camera::Camera() {
 	init_lib();
+	type = sgltk::PERSPECTIVE;
 	ident = glm::mat4(1);
 
-	this->pos = glm::vec4(0, 0, 1, 1);
-	this->dir = glm::vec4(0, 0, -1, 1);
-	this->up = glm::vec4(0, 1, 0, 1);
+	pos = glm::vec4(0, 0, 1, 1);
+	dir = glm::vec4(0, 0, -1, 1);
+	up = glm::vec4(0, 1, 0, 1);
 	right = glm::vec4(glm::cross(glm::vec3(dir), glm::vec3(up)), 1);
-	this->fov = 70.0f;
-	this->near_plane = 1.0f;
-	this->far_plane = 800.0f;
+	fov = 70.0f;
+	near_plane = 1.0f;
+	far_plane = 800.0f;
+
+	update_view_matrix();
+	update_projection_matrix(fov, 640, 480, near_plane, far_plane);
+}
+
+Camera::Camera(sgltk::CAMERA_TYPE type) {
+	init_lib();
+	this->type = type;
+	ident = glm::mat4(1);
+
+	pos = glm::vec4(0, 0, 1, 1);
+	dir = glm::vec4(0, 0, -1, 1);
+	up = glm::vec4(0, 1, 0, 1);
+	right = glm::vec4(glm::cross(glm::vec3(dir), glm::vec3(up)), 1);
+	fov = 70.0f;
+	near_plane = 1.0f;
+	far_plane = 800.0f;
 
 	update_view_matrix();
 	update_projection_matrix(fov, 640, 480, near_plane, far_plane);
@@ -18,13 +36,34 @@ Camera::Camera() {
 
 Camera::Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up) {
 	init_lib();
-	Camera(pos, dir, up, 70.0f, 640, 480, 1.0f, 800.0f);
+	type = sgltk::PERSPECTIVE;
 	ident = glm::mat4(1);
 
-	right = glm::vec4(glm::cross(glm::vec3(dir), glm::vec3(up)), 1);
-	this->fov = 70.0f;
-	this->near_plane = 1.0f;
-	this->far_plane = 800.0f;
+	this->pos = glm::vec4(pos, 1);
+	this->dir = glm::vec4(dir, 1);
+	this->up = glm::vec4(up, 1);
+	right = glm::vec4(glm::cross(dir, up), 1);
+	fov = 70.0f;
+	near_plane = 1.0f;
+	far_plane = 800.0f;
+
+	update_view_matrix();
+	update_projection_matrix(fov, 640, 480, near_plane, far_plane);
+}
+
+Camera::Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up,
+	       sgltk::CAMERA_TYPE type) {
+	init_lib();
+	this->type = type;
+	ident = glm::mat4(1);
+
+	this->pos = glm::vec4(pos, 1);
+	this->dir = glm::vec4(dir, 1);
+	this->up = glm::vec4(up, 1);
+	right = glm::vec4(glm::cross(dir, up), 1);
+	fov = 70.0f;
+	near_plane = 1.0f;
+	far_plane = 800.0f;
 
 	update_view_matrix();
 	update_projection_matrix(fov, 640, 480, near_plane, far_plane);
@@ -33,6 +72,25 @@ Camera::Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up) {
 Camera::Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up, float fov,
 	       float width, float height, float near_plane, float far_plane) {
 	init_lib();
+	type = sgltk::PERSPECTIVE;
+	ident = glm::mat4(1);
+
+	this->pos = glm::vec4(pos, 1);
+	this->dir = glm::vec4(dir, 1);
+	this->up = glm::vec4(up, 1);
+	right = glm::vec4(glm::cross(dir, up), 1);
+	this->fov = fov;
+	this->near_plane = near_plane;
+	this->far_plane = far_plane;
+
+	update_view_matrix();
+	update_projection_matrix(fov, width, height, near_plane, far_plane);
+}
+
+Camera::Camera(glm::vec3 pos, glm::vec3 dir, glm::vec3 up, float fov,
+	       float width, float height, float near_plane, float far_plane, sgltk::CAMERA_TYPE type) {
+	init_lib();
+	this->type = type;
 	ident = glm::mat4(1);
 
 	this->pos = glm::vec4(pos, 1);
@@ -61,14 +119,26 @@ void Camera::update_projection_matrix(float width, float height) {
 }
 
 void Camera::update_projection_matrix(float fov, float width, float height) {
+	this->fov = fov;
+
 	update_projection_matrix(fov, width, height, near_plane, far_plane);
 }
 
 void Camera::update_projection_matrix(float fov, float width, float height,
 				      float near_plane, float far_plane) {
-	projection_matrix = glm::perspective(fov,
-				(GLfloat)width/(GLfloat)height,
-				near_plane, far_plane);
+	this->fov = fov;
+	this->near_plane = near_plane;
+	this->far_plane = far_plane;
+
+	if(type & sgltk::PERSPECTIVE) {
+		projection_matrix_persp = glm::perspective(fov,
+					  (GLfloat)width/(GLfloat)height,
+					  near_plane, far_plane);
+	}
+	if(type & sgltk::ORTHOGRAPHIC) {
+		projection_matrix_ortho = glm::ortho(0.0f, width, 0.0f, height,
+						     near_plane, far_plane);
+	}
 }
 
 void Camera::move_up(float delta) {
