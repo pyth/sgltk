78b485e611163bd045e405dde2f5ee45bbea4daa
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-07-08 23:38:08 +0200

    OpenGL version guards

--- src/window.cpp
+++ src/window.cpp
@@ -14,11 +14,24 @@ Window::Window(const char* title, int res_x, int res_y,
 
 	int glmaj = gl_maj;
 	int glmin = gl_min;
+	if(gl_min < 0) {
+		App::error_string.push_back("Minor version number cannot be"
+			"negative. Defaulting to version x.0");
+		glmin = 0;
+	}
 	if(gl_maj < 3) {
 		App::error_string.push_back("sgltk requires opengl version 3.0"
 			"or newer. Defaulting to version 3.0");
 		glmaj = 3;
 		glmin = 0;
+	} else if(gl_maj == 3 && gl_min > 3) {
+		App::error_string.push_back("Did you mean 3.3?"
+			"Defaulting to version 3.3");
+		glmin = 3;
+	} else if(gl_maj == 4 && gl_min > 5) {
+		App::error_string.push_back("Did you mean 4.5?"
+			"Defaulting to version 4.5");
+		glmin = 5;
 	}
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, glmaj);
