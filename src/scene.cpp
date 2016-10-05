pyth/sgltk

commit ccf499e3ce2e6f36eff751ec3c62444b2d42442a

updated the way view and projection matrices are set for meshes and scenes

src/scene.cpp
@@ -7,8 +7,6 @@ std::vector<std::string> Scene::paths = {"./"};
 Scene::Scene() {
 	scene = NULL;
 	shader = NULL;
-	view_matrix = NULL;
-	projection_matrix = NULL;
 
 	position_name = "pos_in";
 	normal_name = "norm_in";
@@ -93,13 +91,25 @@ void Scene::compute_bounding_box() {
 	}
 }
 
-void Scene::setup_camera(glm::mat4 *view_matrix,
+bool Scene::setup_camera(glm::mat4 *view_matrix,
 			 glm::mat4 *projection_matrix) {
-	this->view_matrix = view_matrix;
-	this->projection_matrix = projection_matrix;
+	bool ret;
 	for(Mesh *mesh : meshes) {
-		mesh->setup_camera(view_matrix, projection_matrix);
+		ret = mesh->setup_camera(view_matrix, projection_matrix);
+		if(!ret)
+			return false;
 	}
+	return true;
+}
+
+bool Scene::setup_camera(Camera *camera, CAMERA_TYPE type) {
+	bool ret;
+	for(Mesh *mesh : meshes) {
+		ret = mesh->setup_camera(camera);
+		if(!ret)
+			return false;
+	}
+	return true;
 }
 
 void Scene::set_position_name(std::string name) {
@@ -161,6 +171,7 @@ void sgltk::Scene::set_bone_array_name(std::string name) {
 void Scene::setup_shader(Shader *shader) {
 	this->shader = shader;
 	for(Mesh *mesh : meshes) {
+		mesh->setup_shader(shader);
 		set_vertex_attribute(mesh);
 	}
 }
@@ -340,38 +351,6 @@ Mesh *Scene::create_mesh(unsigned int index) {
 	}
 	mesh_tmp->compute_bounding_box(position, 0);
 	mesh_tmp->attach_index_buffer(indices);
-	mesh_tmp->setup_shader(shader);
-	mesh_tmp->setup_camera(view_matrix, projection_matrix);
-
-	//************************************
-	// Set attribute pointers
-	//************************************
-	/*mesh_tmp->set_vertex_attribute(position_name, pos_buf, 4, GL_FLOAT,
-				0, 0);
-	mesh_tmp->set_vertex_attribute(normal_name, norm_buf, 3, GL_FLOAT,
-				0, 0);
-	mesh_tmp->set_vertex_attribute(tangent_name, tan_buf, 4, GL_FLOAT,
-				0, 0);
-	mesh_tmp->set_vertex_attribute(bone_ids_name, id_buf, BONES_PER_VERTEX,
-				GL_INT, 0, 0);
-	mesh_tmp->set_vertex_attribute(bone_weights_name, weight_buf,
-				BONES_PER_VERTEX, GL_FLOAT, 0, 0);
-
-	if(num_uv) {
-		for(unsigned int i = 0; i < num_uv; i++) {
-			mesh_tmp->set_vertex_attribute(
-				texture_coordinates_name + std::to_string(i),
-				tc_buf, 3, GL_FLOAT, 0,
-				(void *)(long)(i * mesh->mNumVertices));
-		}
-	}
-	if(num_col) {
-		for(unsigned int i = 0; i < num_col; i++) {
-			mesh_tmp->set_vertex_attribute(
-				color_name + std::to_string(i), col_buf, 4, GL_FLOAT, 0,
-				(void *)(long)(i * mesh->mNumVertices));
-		}
-	}*/
 	if(shader)
 		set_vertex_attribute(mesh_tmp);
 

src/scene.h
@@ -37,9 +37,6 @@ class Scene {
 	std::string bone_ids_name;
 	std::string bone_weights_name;
 	std::string bone_array_name;
-	
-	glm::mat4 *view_matrix;
-	glm::mat4 *projection_matrix;
 
 	double ticks_per_second;
 	std::vector<Bone> bones;
@@ -94,9 +91,18 @@ class Scene {
 		 *	  used by the meshes in the scene
 		 * @param view_matrix The view matrix
 		 * @param projection_matrix The projection matrix
+		 * @return Returns true if both pointers are not NULL pointers, flase otherwise
 		 */
-		EXPORT void setup_camera(glm::mat4 *view_matrix,
-				  glm::mat4 *projection_matrix);
+		EXPORT bool setup_camera(glm::mat4 *view_matrix,
+					 glm::mat4 *projection_matrix);
+		/**
+		 * @brief Sets up the view and projection matrices that will be
+		 *	  used by the meshes in the scene
+		 * @param camera The camera to use
+		 * @param type The type of projection to use if the camera has more than one type
+		 * @return Returns true on success, false otherwise
+		 */
+		EXPORT bool setup_camera(Camera *camera, sgltk::CAMERA_TYPE type = sgltk::PERSPECTIVE);
 		/**
 		 * @brief Sets the position vertex attribute name in the shader
 		 * @param name The new vertex attribute name. An empty string resets

src/mesh.cpp
@@ -70,10 +70,39 @@ void Mesh::setup_shader(Shader *shader) {
 	this->shader = shader;
 }
 
-void Mesh::setup_camera(glm::mat4 *view_matrix,
+bool Mesh::setup_camera(glm::mat4 *view_matrix,
 				glm::mat4 *projection_matrix) {
+	if(!view_matrix || !projection_matrix)
+		return false;
+
 	this->view_matrix = view_matrix;
 	this->projection_matrix = projection_matrix;
+	return true;
+}
+
+bool Mesh::setup_camera(Camera *camera, CAMERA_TYPE type) {
+	this->view_matrix = &camera->view_matrix;
+	if(camera->type == ORTHOGRAPHIC) {
+		this->projection_matrix = &camera->projection_matrix_ortho;
+	} else if(camera->type == PERSPECTIVE) {
+		this->projection_matrix = &camera->projection_matrix_persp;
+	} else if(camera->type == INF_PERSPECTIVE) {
+		this->projection_matrix = &camera->projection_matrix_persp_inf;
+	} else if(camera->type & type){
+		//camera has more than one type
+		if(type == ORTHOGRAPHIC) {
+			this->projection_matrix = &camera->projection_matrix_ortho;
+		}
+		if(type == PERSPECTIVE) {
+			this->projection_matrix = &camera->projection_matrix_persp;
+		}
+		if(type == INF_PERSPECTIVE) {
+			this->projection_matrix = &camera->projection_matrix_persp_inf;
+		}
+	} else {
+		return false;
+	}
+	return true;
 }
 
 void Mesh::set_model_matrix_name(std::string name) {

src/mesh.h
@@ -3,6 +3,7 @@
 
 #include "app.h"
 #include "shader.h"
+#include "camera.h"
 #include "texture.h"
 
 namespace sgltk {
@@ -337,9 +338,18 @@ public:
 	 * 	  by the mesh
 	 * @param view_matrix The view matrix
 	 * @param projection_matrix The projection matrix
+	 * @return Returns true if both pointers are not NULL pointers, flase otherwise
 	 */
-	EXPORT void setup_camera(glm::mat4 *view_matrix,
-			  glm::mat4 *projection_matrix);
+	EXPORT bool setup_camera(glm::mat4 *view_matrix,
+				 glm::mat4 *projection_matrix);
+	/**
+	 * @brief Sets up the view and projection matrices that will be used
+	 * 	  by the mesh
+	 * @param camera The camera to use
+	 * @param type The projection type to be used if the camera has more than one type
+	 * @return Returns true on success, false otherwise
+	 */
+	EXPORT bool setup_camera(Camera *camera, sgltk::CAMERA_TYPE = sgltk::PERSPECTIVE);
 	/**
 	 * @brief Sets the name of the model matrix in the shader
 	 * @param name The name of the model matrix.

commit 07ba0fd4171e8316a2729e8f885bba7d4eff5635

fixed errors introduced in commit ccf499e3ce2e6f36eff751ec3c62444b2d42442a

src/scene.cpp
@@ -8,6 +8,9 @@ Scene::Scene() {
 	scene = NULL;
 	shader = NULL;
 
+	view_matrix = NULL;
+	projection_matrix = NULL;
+
 	position_name = "pos_in";
 	normal_name = "norm_in";
 	tangent_name = "tang_in";
@@ -99,6 +102,8 @@ bool Scene::setup_camera(glm::mat4 *view_matrix,
 		if(!ret)
 			return false;
 	}
+	this->view_matrix = view_matrix;
+	this->projection_matrix = projection_matrix;
 	return true;
 }
 
@@ -109,6 +114,15 @@ bool Scene::setup_camera(Camera *camera, CAMERA_TYPE type) {
 		if(!ret)
 			return false;
 	}
+	view_matrix = view_matrix;
+	if(type == PERSPECTIVE)
+		projection_matrix = &camera->projection_matrix_persp;
+	else if(type == INF_PERSPECTIVE)
+		projection_matrix = &camera->projection_matrix_persp_inf;
+	else if(type == ORTHOGRAPHIC)
+		projection_matrix = &camera->projection_matrix_ortho;
+	else
+		return false;
 	return true;
 }
 
@@ -351,8 +365,12 @@ Mesh *Scene::create_mesh(unsigned int index) {
 	}
 	mesh_tmp->compute_bounding_box(position, 0);
 	mesh_tmp->attach_index_buffer(indices);
-	if(shader)
+	if(shader) {
+		mesh_tmp->setup_shader(shader);
 		set_vertex_attribute(mesh_tmp);
+	}
+	if(view_matrix && projection_matrix)
+		mesh_tmp->setup_camera(view_matrix, projection_matrix);
 
 	//************************************
 	// Materials

src/scene.h
@@ -29,6 +29,9 @@ class Scene {
 	const aiScene *scene;
 	Shader *shader;
 
+	glm::mat4 *view_matrix;
+	glm::mat4 *projection_matrix;
+
 	std::string position_name;
 	std::string normal_name;
 	std::string tangent_name;