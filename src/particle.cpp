f9121c4b52401faa70af118c62b3009e68a859eb
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-10-25 05:54:27 +0200

    particle system

--- src/particle.cpp
+++ src/particle.cpp
@@ -2,21 +2,75 @@
 
 using namespace sgltk;
 
-Particles::Particles(int number, glm::vec3 origin, glm::vec3 velocity) {
-	this->number = number;
-	this->origin = origin;
-	this->velocity = velocity;
-	this->origin_offset = glm::vec3(0);
-	this->velocity_offset = glm::vec3(0);
-}
-
-Particles::Particles(int number, glm::vec3 origin, glm::vec3 velocity, glm::vec3 origin_offset, glm::vec3 velocity_offset) {
-	this->number = number;
-	this->origin = origin;
-	this->velocity = velocity;
-	this->origin_offset = origin_offset;
-	this->velocity_offset = velocity_offset;
+Particles::Particles() {
+	timer.start();
 }
 
 Particles::~Particles() {
+}
+
+void Particles::resize(unsigned int number) {
+	this->number = number;
+	position.resize(number);
+	velocity.resize(number);
+	lifetime.resize(number);
+	indices.resize(number);
+	for(unsigned int i = 0; i < number; i++) {
+		indices[i] = i;
+	}
+	pos_buf = mesh.attach_vertex_buffer<glm::vec3>(position, GL_DYNAMIC_DRAW);
+	vel_buf = mesh.attach_vertex_buffer<glm::vec3>(velocity, GL_DYNAMIC_DRAW);
+	life_buf = mesh.attach_vertex_buffer<glm::vec2>(lifetime, GL_DYNAMIC_DRAW);
+	mesh.attach_index_buffer(indices);
+	mesh.set_vertex_attribute("position_in", pos_buf, 3, GL_FLOAT, 0, 0);
+	mesh.set_vertex_attribute("velocity_in", vel_buf, 3, GL_FLOAT, 0, 0);
+	mesh.set_vertex_attribute("lifetime_in", life_buf, 2, GL_FLOAT, 0, 0);
+}
+
+void Particles::setup_shader(Shader *shader) {
+	mesh.setup_shader(shader);
+}
+
+void Particles::setup_camera(Camera *camera, CAMERA_TYPE type) {
+	mesh.setup_camera(camera, type);
+}
+
+void Particles::setup_camera(glm::mat4 *view_matrix,
+				glm::mat4 *projection_matrix) {
+
+	mesh.setup_camera(view_matrix, projection_matrix);
+}
+
+bool Particles::add_particle(glm::vec3 position,
+				glm::vec3 velocity,
+				float lifetime) {
+
+	float time = timer.get_time();
+	for(unsigned int i = 0; i < number; i++) {
+		if(this->lifetime[i][0] + this->lifetime[i][1] < time) {
+			this->position[i] = position;
+			this->velocity[i] = velocity;
+			this->lifetime[i] = glm::vec2(time, lifetime);
+			break;
+		}
+	}
+	return true;
+}
+
+void Particles::update() {
+	mesh.replace_buffer_data<glm::vec3>(pos_buf, position);
+	mesh.replace_buffer_data<glm::vec3>(vel_buf, velocity);
+	mesh.replace_buffer_data<glm::vec2>(life_buf, lifetime);
+	mesh.set_vertex_attribute("position_in", pos_buf, 3, GL_FLOAT, 0, 0);
+	mesh.set_vertex_attribute("velocity_in", vel_buf, 3, GL_FLOAT, 0, 0);
+	mesh.set_vertex_attribute("lifetime_in", life_buf, 2, GL_FLOAT, 0, 0);
+}
+
+void Particles::draw() {
+	if(!mesh.shader) {
+		App::error_string.push_back("No shader set.");
+	}
+	mesh.shader->bind();
+	mesh.shader->set_uniform_float("time", timer.get_time());
+	mesh.draw(GL_POINTS);
 }
--- src/particle.h
+++ src/particle.h
@@ -2,23 +2,85 @@
 #define __PARTICLE_H__
 
 #include "app.h"
+#include "mesh.h"
+#include "timer.h"
+#include "shader.h"
+#include "camera.h"
 
 namespace sgltk {
 
+/**
+ * @class Particles
+ * @brief The implementation of a particle system using the mesh class
+ */
 class Particles {
-	int number;
-	glm::vec3 origin;
-	glm::vec3 velocity;
-	glm::vec3 origin_offset;
-	glm::vec3 velocity_offset;
+	Mesh mesh;
+	Timer timer;
+	unsigned int pos_buf;
+	unsigned int vel_buf;
+	unsigned int life_buf;
+	unsigned int number;
 
+	std::vector<glm::vec3> position;
+	std::vector<glm::vec3> velocity;
+	std::vector<glm::vec2> lifetime;
+	std::vector<unsigned int> indices;
 public:
-	Particles(int number, glm::vec3 origin, glm::vec3 velocity);
-	Particles(int number, glm::vec3 origin, glm::vec3 velocity, glm::vec3 origin_offset, glm::vec3 velocity_offset);
+	Particles();
 	~Particles();
 
-	void render();
+	/**
+	 * @brief Sets the number of the particles in the system
+	 * @param number The new number of particles
+	 */
+	void resize(unsigned int number);
+	/**
+	 * @brief Specifies the shader to use to render the particles
+	 * @param shader The shader to be used to render the particles
+	 */
+	void setup_shader(sgltk::Shader *shader);
+	/**
+	 * @brief Sets up the view and projection matrices that will be
+	 *	  used to render the particles
+	 * @param camera The camera to be used to draw the particles
+	 * @param type The type of the camera
+	 */
+	void setup_camera(sgltk::Camera *camera,
+			  sgltk::CAMERA_TYPE type = sgltk::PERSPECTIVE);
+	/**
+	 * @brief Sets up the view and projection matrices that will be
+	 *	  used to render the particles
+	 * @param view_matrix The view matrix
+	 * @param projection_matrix The projection matrix
+	 */
+	void setup_camera(glm::mat4 *view_matrix,
+			  glm::mat4 *projection_matrix);
+	/**
+	 * @brief Attaches a texture to the particles
+	 */
+	void attach_texture(const std::string& name,
+			    sgltk::Texture *texture);
+	/**
+	 * @brief Adds a new particle to the system if the particle buffer has an empty space
+	 * @param position The position of the new particle
+	 * @param velocity The velocity vector of the new particle
+	 * @param lifetime The lifetime of the particle in seconds 
+	 * @return Returns true if the particle was successfully added to the buffer, 
+	 * 	false otherwise
+	 */
+	bool add_particle(glm::vec3 position,
+			  glm::vec3 velocity,
+			  float lifetime);
+	/**
+	 * @brief Updates the particle buffers
+	 */
+	void update();
+	/**
+	 * @brief Draws the particles
+	 */
+	void draw();
 };
+
 }
 
 #endif
