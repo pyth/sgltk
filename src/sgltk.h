660894851fd38ba08796fabb0e8b267df8ba2700
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2017-07-24 15:04:36 +0200

    added a unified header

---
+++ src/sgltk.h
@@ -0,0 +1,21 @@
+#ifndef __SGLTK_H__
+#define __SGLTK_H__
+
+#include "config.h"
+#include "app.h"
+#include "timer.h"
+#include "buffer.h"
+#include "camera.h"
+#include "image.h"
+#include "texture.h"
+#include "shader.h"
+#include "renderbuffer.h"
+#include "framebuffer.h"
+#include "mesh.h"
+#include "model.h"
+#include "particle.h"
+#include "gamepad.h"
+#include "joystick.h"
+#include "window.h"
+
+#endif //__SGLTK_H__
