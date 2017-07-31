2f7591b12a491e19ee5457c93bdfd3fb887e455b
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2017-07-31 04:09:26 +0200

    new particles can now be added separately or in bulk

--- src/particle.cpp
+++ src/particle.cpp
@@ -51,7 +51,7 @@ bool Particles::add_particle(glm::vec3 position,
 				glm::vec3 velocity,
 				float lifetime) {
 
-	float time = (float)timer.get_time();
+	float time = (float)timer.get_time_s();
 	for(unsigned int i = 0; i < number; i++) {
 		if(this->lifetime[i][0] + this->lifetime[i][1] < time) {
 			this->position[i] = position;
@@ -63,7 +63,29 @@ bool Particles::add_particle(glm::vec3 position,
 	return true;
 }
 
-void Particles::update() {
+bool Particles::add_particle_immediately(glm::vec3 position,
+					glm::vec3 velocity,
+					float lifetime) {
+
+	float time = (float)timer.get_time_s();
+	for(unsigned int i = 0; i < number; i++) {
+		if(this->lifetime[i][0] + this->lifetime[i][1] < time) {
+			this->position[i] = position;
+			this->velocity[i] = velocity;
+			this->lifetime[i] = glm::vec2(time, lifetime);
+			mesh.replace_partial_data<glm::vec3>(pos_buf, i * sizeof(glm::vec3), &this->position[i], 1);
+			mesh.replace_partial_data<glm::vec3>(vel_buf, i * sizeof(glm::vec3), &this->velocity[i], 1);
+			mesh.replace_partial_data<glm::vec2>(life_buf, i * sizeof(glm::vec2), &this->lifetime[i], 1);
+			mesh.set_vertex_attribute("position_in", pos_buf, 3, GL_FLOAT, 0, 0);
+			mesh.set_vertex_attribute("velocity_in", vel_buf, 3, GL_FLOAT, 0, 0);
+			mesh.set_vertex_attribute("lifetime_in", life_buf, 2, GL_FLOAT, 0, 0);
+			break;
+		}
+	}
+	return true;
+}
+
+void Particles::update_all() {
 	mesh.replace_buffer_data<glm::vec3>(pos_buf, position);
 	mesh.replace_buffer_data<glm::vec3>(vel_buf, velocity);
 	mesh.replace_buffer_data<glm::vec2>(life_buf, lifetime);
@@ -77,6 +99,6 @@ void Particles::draw() {
 		App::error_string.push_back("No shader set.");
 	}
 	mesh.shader->bind();
-	mesh.shader->set_uniform_float("time", (float)timer.get_time());
+	mesh.shader->set_uniform_float("time", (float)timer.get_time_s());
 	mesh.draw(GL_POINTS);
 }
